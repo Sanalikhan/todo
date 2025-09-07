8612ab4fabfe5f0f3dbdc2e7b900914407c05973
Sana Ali Khan <sanalikhan0110@gmail.com>
2025-09-07 17:49:37 +0300

    changed next.congif.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://todo-a61k.vercel.app/api:path*',
+          },
+        ]
+      },
 };
 
 module.exports = nextConfig;
